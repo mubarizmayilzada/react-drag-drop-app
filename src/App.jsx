25cf28940c54948859e6e7c5e85f54879223c418
mubarizmayilzade <mubarizbm@code.edu.az>
2022-02-28 02:39:56 +0400

    drag-drop-app done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './app.scss';
+import DragDrop from './components/dragDrop';
+
+function App() {
+  return (
+    <div style={{padding:'50px'}}>
+      <h1 style={{marginBottom: '20px'}}>
+        M2N
+      </h1>
+      <DragDrop/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card/index.jsx
@@ -0,0 +1,8 @@
+import './card.scss'
+const Card = (props) => {
+  return (
+    <div className='card'>{props.children}</div>
+  )
+}
+
+export default Card
